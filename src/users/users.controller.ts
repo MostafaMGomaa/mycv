cd29a5f1ad6e8f97c128a7484619a75e108086e6
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-01-19 10:41:11 +0200

    impl: signuo and get all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    return 'It work in the pipe';
+    console.log(body.email, body.password);
+    this.usersService.create(body.email, body.password);
+    return 'User created Successfully';
+  }
+
+  @Get('/')
+  getAllUsers() {
+    return this.usersService.list();
   }
 }
